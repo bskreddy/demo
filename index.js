e5bb9fc0fd019b8dae25f31b5ce9d3419b2f0bf7
EC2 Default User <ec2-user@ip-172-31-19-208.us-west-2.compute.internal>
2018-12-11 05:50:19 +0000

    default

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.get('/', function (req, res) {
   res.send('sent from prasad branch!');
+  res.send('default running');
 });
 
 var server = app.listen(3000, function () {
